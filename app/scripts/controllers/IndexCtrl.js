e843c48a826542b86773f15fe0c6a63dedd1bc97
AndriyShepitsen <andriy.shepitsen@chicagobusinessintelligence.net>
2014-06-25 14:45:08 -0500

    Initial Project

--- app/scripts/controllers/IndexCtrl.js
+++ app/scripts/controllers/IndexCtrl.js
@@ -1,10 +1,11 @@
 var IndexCtrl = (function () {
-    function IndexCtrl($scope, DataService) {
-        this.$scope = $scope;
-        this.DataService = DataService;
-        $scope.error = false;
+	function IndexCtrl($scope, DataService) {
+		this.$scope = $scope;
+		this.DataService = DataService;
+		$scope.error = false;
 
-        $scope.index = this;
-    }
-    return IndexCtrl;
+		$scope.index = this;
+	}
+
+	return IndexCtrl;
 })();
--- app/scripts/models/IBreederProfile.js
+++ app/scripts/models/IBreederProfile.js
@@ -1,25 +1,29 @@
 var Feedback = (function () {
-    function Feedback() {
-        this.ClientName = "";
-        this.FeedbackBody = "";
-    }
-    return Feedback;
+	function Feedback() {
+		this.ClientName = "";
+		this.FeedbackBody = "";
+	}
+
+	return Feedback;
 })();
 
 var Gallery = (function () {
-    function Gallery() {
-    }
-    return Gallery;
+	function Gallery() {
+	}
+
+	return Gallery;
 })();
 var BreederProfile = (function () {
-    function BreederProfile() {
-    }
-    return BreederProfile;
+	function BreederProfile() {
+	}
+
+	return BreederProfile;
 })();
 
 var Litter = (function () {
-    function Litter() {
-        this.Photos = [];
-    }
-    return Litter;
+	function Litter() {
+		this.Photos = [];
+	}
+
+	return Litter;
 })();
