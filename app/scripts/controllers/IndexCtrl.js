06606001e9931620ebb96460f5ebb60bf73abb01
AndriyShepitsen <andriy.shepitsen@chicagobusinessintelligence.net>
2014-06-25 14:34:15 -0500

    Initial Project

---
+++ app/scripts/controllers/IndexCtrl.js
@@ -0,0 +1,11 @@
+var IndexCtrl = (function () {
+	function IndexCtrl($scope, DataService) {
+		this.$scope = $scope;
+		this.DataService = DataService;
+		$scope.error = false;
+
+		$scope.index = this;
+	}
+
+	return IndexCtrl;
+})();
